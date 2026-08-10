0c98c27d6c6936921cb31f1ea94a6ed37376a625
nagarajasri <nagarajasrib054@gmail.com>
2026-08-10 11:39:07 +0530

    Add files via upload

---
+++ exp 1.js
@@ -0,0 +1,35 @@
+const express = require('express');
+
+const app = express();
+
+// Home Route
+app.get('/', (req, res) => {
+    res.send('Welcome to Home Page!');
+});
+
+// Route Parameter
+app.get('/user/:userId', (req, res) => {
+    const userId = req.params.userId;
+    res.send('User ID: ' + userId);
+});
+
+// Query Parameters
+app.get('/search', (req, res) => {
+    const term = req.query.term;
+    const userId = req.query.userId;
+
+    if (term) {
+        res.send('You searched for: ' + term);
+    } 
+    else if (userId) {
+        res.send('You searched for: ' + userId);
+    } 
+    else {
+        res.send('You did not search for anything');
+    }
+});
+
+// Start Server
+app.listen(3000, () => {
+    console.log('Server running at http://localhost:3000');
+});
